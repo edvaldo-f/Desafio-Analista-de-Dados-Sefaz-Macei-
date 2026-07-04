237d8468ba2645be64d604322eec357b3cf3c11f
edvaldo-f <neto1810.en@gmail.com>
2026-07-03 23:24:25 -0300

    feat: implementado o script que otimiza os dados no formato Parquet.
    Para um melhor armazenamento e leitura dos dados.
    Outros arquivos como requirements e RESOLUCAO.md já foram atualizados por conta disso.

--- RESOLUCAO.md
+++ RESOLUCAO.md
@@ -5,8 +5,12 @@ O presente arquivo serve para salvar minhas conclusões, análises, metodologia 
 > Pastas e arquivos como "dados_extraidos/" e ".parquet" não foram versionados nesse repositório. Como presente pelas linhas descomentadas no gitignore.
 
 ## Metodologia
+...
 
-...em produção
+### Porque foi utilizado o formato Parquet?
+    Para essa resolução, o formato Parquet foi o adotado pela sua compactação dos dados, permitindo um armazenamento melhor. Tal formato é amplamente usado para facilitar o armazenamento de conjuntos de dados afim de economizar armazenamento e tempo de leitura, visto que ele organiza os arquivos em colunas ao invés de linhas, oque também permite queries analíticas melhores.
+
+...
 
 ## Análises
 ...em produção
---
+++ scripts/otimizar_formato.py
@@ -0,0 +1,35 @@
+### Script para otimizar o formato do arquivo consolidado dos dados, Passo 3 do desafio.
+
+import pandas as pd
+from pathlib import Path
+
+"""
+==========================================================
+Para essa otimização, será usado a conversão para o formato Parquet.
+Justificativas no RESOLUCAO.md.    
+==========================================================
+"""
+
+# Encontrando a raiz do projeto.
+raiz_do_projeto = Path(__file__).parent.parent
+# Caminho do arquivo consolidado.
+caminho_arquivo_consolidado = raiz_do_projeto / "dados_processados" / "dados_consolidados.csv"
+
+print(f"Lendo o arquivo consolidado em {caminho_arquivo_consolidado}...")
+# Caminho do arquivo otimizado.
+pasta_otimizados = raiz_do_projeto / "dados_otimizados"
+pasta_otimizados.mkdir(parents=True, exist_ok=True)  # Cria a pasta se não existir
+
+
+# Lendo e convertendo o arquivo consolidado para o formato Parquet.
+df_consolidado = pd.read_csv(
+    caminho_arquivo_consolidado, 
+    sep=";", 
+    encoding="latin-1", 
+    decimal=",")
+print(f"Convertendo o arquivo consolidado para o formato Parquet em {pasta_otimizados}...")
+
+caminho_parquet = pasta_otimizados / "dados_otimizados.parquet"
+df_consolidado.to_parquet(caminho_parquet, index=False)
+
+print("Conversão concluída. O arquivo otimizado foi salvo em dados_otimizados/dados_otimizados.parquet.")
